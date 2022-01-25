633752c1ae5ce6e88c68184ef45cceeaabc774fd
Emma Egstad <emma@egstad.com>
2022-01-24 16:14:52 -0800

    cleaning up code

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -5,11 +5,10 @@ test('Should render the header', async () => {
   render(<App />)
 
   const image = screen.getByAltText(/alchemy/i)
-  expect(image).toBeInTheDocument()
-
-  const profileName = await screen.findByText(/Vonta/i)
-  expect(profileName).toBeInTheDocument()
-
   const bannerColor = screen.getByRole('banner', {})
+  const profileName = await screen.findByText(/Vonta/i)
+
+  expect(image).toBeInTheDocument()
+  expect(profileName).toBeInTheDocument()
   expect(bannerColor).toHaveStyle({ background: 'var(--grey)' })
 })
